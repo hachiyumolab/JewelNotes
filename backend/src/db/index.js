df058242b6116a9596eebf28d64d5fc60bd92cb9
momotokyo <sa.rai.to.holygrail@gmail.com>
2025-11-22 16:36:55 +0900

    フロントエンド、バックエンド、インフラ、ぜんぶDockerでコンテナ化！

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -6,11 +6,11 @@ dotenv.config();
 const { Client } = pkg;
 
 export const db = new Client({
-    host: process.env.DATABASE_HOST,
-    port: process.env.DATABASE_PORT,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 });
 
 export async function connectDB() {
